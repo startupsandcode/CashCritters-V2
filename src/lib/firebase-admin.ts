ab98fc9d733a40b24533c7c2b4542d4bc01ff6f1
John Mann <john@startupsandcode.com>
2026-03-14 11:28:58 -0400

    updated for vercel

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,18 +1,22 @@
+import * as admin from 'firebase-admin';
 
-  import * as admin from 'firebase-admin';
-  import * as serviceAccount from '../../firebase-service-account.json';
-  
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    });
-  }
-  
-  const db = admin.firestore();
-  const auth = admin.auth();
-  const storage = admin.storage();
-  const realTimeDb = admin.database();
-  
-  export { admin, db, auth, storage, realTimeDb };
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  });
+}
+
+const db = admin.firestore();
+const auth = admin.auth();
+const storage = admin.storage();
+const realTimeDb = admin.database();
+
+export { admin, db, auth, storage, realTimeDb };
   
